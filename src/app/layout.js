f32282c975a70402323375fdbb299df56869ac6d
Fikret Erkara <fikreterkara_26@hotmail.com>
2024-05-08 11:28:09 +0300

    add input component and update home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
             <div className="hidden sm:inline border-r h-screen">
               <Sidebar />
             </div>
-            <div>{children}</div>
+            <div className="w-2xl flex-1">{children}</div>
             <div className="hidden lg:flex lg:flex-col h-screen p-3 border-l w-[24rem]">
               <div className="sticy top-0 bg-white py-2">
                 <input
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+import Input from '@/components/Input'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='max-w-xl mx-auto border-r border-l min-h-screen'>
+      <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
+        <h2 className='text-xl sm:text-xl font-bold'>Home</h2>
+      </div>
+      <Input />
+    </div>
   )
 }
 
